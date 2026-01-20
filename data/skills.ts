56be5a5ee631b8238ae6608ea2b807aab019aae5
Adeniyiba <adeniyibamise75@gmail.com>
2026-01-20 18:02:57 +0100

    Create skills.ts

---
+++ data/skills.ts
@@ -0,0 +1,39 @@
+export const skillCards = [
+  {
+    title: "Skill Card 1",
+    description: "One sentence description",
+  },
+  {
+    title: "Skill Card 2",
+    description: "One sentence description",
+  },
+  {
+    title: "Skill Card 3",
+    description: "One sentence description",
+  },
+  {
+    title: "Skill Card 4",
+    description: "One sentence description",
+  },
+  {
+    title: "Skill Card 5",
+    description: "One sentence description",
+  },
+  {
+    title: "Skill Card 6",
+    description: "One sentence description",
+  },
+];
+
+export const skillChips = [
+  "Research",
+  "Writing",
+  "Analytics",
+  "Strategy",
+  "Public Speaking",
+  "Product",
+  "Risk",
+  "Web3",
+  "Finance",
+  "Design",
+];
