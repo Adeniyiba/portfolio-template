ce1f8799e4f0da46f76d3021f55ac6cdb9cd2f9d
Adeniyiba <adeniyibamise75@gmail.com>
2026-01-20 17:32:22 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+export default function Page() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
